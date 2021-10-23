e1ef6c0cb0c87e54ddf56d628bd4dbbff2cbb714
AndyDev_ <57963445+AndyDev25@users.noreply.github.com>
2021-10-23 15:54:35 +0200

    added support for blocking and unblocking users

--- src/Messages.ts
+++ src/Messages.ts
@@ -1,4 +1,6 @@
+import { URLSearchParams } from "url";
 import { credentials } from "./User";
+import { AuthError } from "./Errors";
 import fetch from "node-fetch";
 import CookieJar from "../lib/CookieJar";
 import { log } from "../lib/log";
@@ -23,6 +25,10 @@ interface parsedMessage {
 interface messageCtx {
   from: string;
   userBlockURL: string;
+  blockSender: () => void;
+  unblockSender: () => void;
+  uid: string;
+  id: number;
   subject: string;
   date: string;
   messages: string | Array<iMessages>;
@@ -34,13 +40,21 @@ interface iMessages {
   _raw: string;
 }
 
+interface blockedUsers {
+  userName: string;
+  blockID: number;
+  unblockUser: () => void;
+}
+
 class Messages {
   public cookies: CookieJar;
   public credentials: credentials;
+  public blockedUsers: blockedUsers[] | [];
 
   constructor() {
     this.cookies = null;
     this.credentials = null;
+    this.blockedUsers = [];
   }
 
   async getMessages(page = 0): Promise<requestedMessages> {
@@ -96,6 +110,8 @@ class Messages {
     const date = /<td><strong>Dátum:<\/strong><\/td><td>(.*?)<\/td>/gm.exec(html)[1];
     const Subject = /<td><strong>Predmet:<\/strong><\/td><td>(.*?)<\/td>/gm.exec(html)[1];
     const singleMessage = /<\/table>(.*?)</gms.exec(html)[1];
+    const uid = new URLSearchParams("https://www.zborovna.sk/novinky/" + blockURL).get("uid");
+    const id = Number(new URLSearchParams(messageURL).get("id"));
     const messages = [...html.matchAll(contextRegEx)].map(ctx => {
       const profileImageURL = ctx.groups.profileImage.split(";").join("&");
       let message: string | any = ctx.groups.message;
@@ -114,14 +130,78 @@ class Messages {
         _raw,
       };
     });
+    // console.log(from.substring(0, from.lastIndexOf(",")));
 
     return {
       from: from.substring(0, from.lastIndexOf(",")),
       userBlockURL: "https://www.zborovna.sk/novinky/" + blockURL,
+      uid,
+      id,
+      blockSender: () => {
+        fetch("https://www.zborovna.sk/novinky/" + blockURL, {
+          headers: {
+            cookie: `${this.cookies.getCookie("PHPSESSID")}; ${this.cookies.getCookie("TestCookie")}`,
+          },
+          method: "GET",
+        }).then(async res => {
+          const url = res.url;
+          if (url.includes("login_form.php")) throw new AuthError("Invalid credentials or cookies!");
+          log(`§33[ZborovnaAPI] §37User: ${from.substring(0, from.indexOf(","))} with uid: ${uid} was successfully blocked.`);
+          await this.getBlockedUsers();
+        });
+      },
+      unblockSender: () => {
+        const user = this.blockedUsers.find((user: blockedUsers) => user?.userName === from.substring(0, from.indexOf(",")));
+        if (!user) return log(`§33[ZborovnaAPI] §37User: ${from.substring(0, from.indexOf(","))} isn't blocked.`);
+
+        fetch("https://www.zborovna.sk/novinky/messages.php", {
+          headers: {
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            cookie: `${this.cookies.getCookie("PHPSESSID")}; ${this.cookies.getCookie("TestCookie")}`,
+          },
+          body: `id=${user.blockID}&action=delete_nastavenie&folder=5&filter=&sub_id=&sub_user_id=&strana=0`,
+          method: "POST",
+        }).then(res => {
+          log(`§33[ZborovnaAPI] §37User: ${user.userName} was successfully unblocked!`);
+        });
+      },
       subject: Subject,
       date,
       messages: singleMessage?.trim() || messages,
     };
+  }
+
+  async getBlockedUsers(): Promise<void> {
+    const response = await fetch("https://www.zborovna.sk/novinky/messages.php?folder=5", {
+      headers: {
+        cookie: `${this.cookies.getCookie("PHPSESSID")}; ${this.cookies.getCookie("TestCookie")}`,
+      },
+    });
+    const html = await response.text();
+    if (/V nastaveniach sa nenachádzajú žiadni blokovaní používatelia\./gm.test(html)) return;
+    const blockedUsers = [...html.matchAll(/<td style="padding:4px" title="(.*?)"><strong>.*?<\/strong><\/td>.*?sprava(\d+)/gms)].map(
+      user => {
+        const [_, userName, id] = user;
+        return {
+          userName,
+          blockID: Number(id),
+          unblockUser: () => {
+            fetch("https://www.zborovna.sk/novinky/messages.php", {
+              headers: {
+                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                cookie: `${this.cookies.getCookie("PHPSESSID")}; ${this.cookies.getCookie("TestCookie")}`,
+              },
+              body: `id=${Number(id)}&action=delete_nastavenie&folder=5&filter=&sub_id=&sub_user_id=&strana=0`,
+              method: "POST",
+            }).then(res => {
+              log(`§33[ZborovnaAPI] §37User: ${userName} was successfully unblocked!`);
+            });
+          },
+        };
+      }
+    );
+
+    this.blockedUsers = blockedUsers;
   }
 
   static baseURL(page = 0) {
